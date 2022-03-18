874e041bea437b390da430fa0a965df3dcebc529
maurici3 <carusojr@outlook.com>
2022-03-18 11:37:59 -0300

    feat: global styles and polished

--- gitcollection/src/pages/Dashboard/index.tsx
+++ gitcollection/src/pages/Dashboard/index.tsx
@@ -1,7 +1,18 @@
 import React from 'react';
 
-import { Title } from './styles';
+import { Title, Form } from './styles';
+import logo from '../../assets/svgs/logo.svg';
 
 export const Dashboard: React.FC = () => {
-  return <Title>Dashboard</Title>;
+  return (
+    <>
+      <img src={logo} alt="GitCollection" />
+      <Title>Catálogo de repositórios do GitHub</Title>
+
+      <Form>
+        <input placeholder="username/repositorie_name" />
+        <button type="submit">Buscar</button>
+      </Form>
+    </>
+  );
 };
