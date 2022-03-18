7dc33a103e267f1669c3455423d0d3dc080ea721
maurici3 <carusojr@outlook.com>
2022-03-18 11:11:52 -0300

    feat: add styled components and r router dom

--- gitcollection/src/App.tsx
+++ gitcollection/src/App.tsx
@@ -1,11 +1,14 @@
 import React from 'react';
+import { BrowserRouter } from 'react-router-dom';
 
-function App() {
+import { Routes } from './routes';
+
+const App: React.FC = () => {
   return (
-    <div className="App">
-      <h1>Olá Dev</h1>
-    </div>
+    <BrowserRouter>
+      <Routes />
+    </BrowserRouter>
   );
-}
+};
 
 export default App;
