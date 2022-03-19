802594b7c3208a5a51964811fac86b9aaa1da0fe
maurici3 <carusojr@outlook.com>
2022-03-19 15:44:41 -0300

    feat: added repositories and list page

--- gitcollection/src/pages/Dashboard/index.tsx
+++ gitcollection/src/pages/Dashboard/index.tsx
@@ -1,10 +1,10 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect, useRef } from 'react';
 import { FiChevronRight } from 'react-icons/fi';
+import { Link } from 'react-router-dom';
 
 import { api } from '../../services/api';
-import { Title, Form, Repos } from './styles';
+import { Title, Form, Repos, Error } from './styles';
 import logo from '../../assets/svg/logo.svg';
-import repoImg from '../../assets/img/repo-img.jpg';
 
 interface GitHubRepository {
   full_name: string;
@@ -16,8 +16,17 @@ interface GitHubRepository {
 }
 
 export const Dashboard: React.FC = () => {
-  const [repos, setRepos] = useState<GitHubRepository[]>([]);
+  const [repos, setRepos] = useState<GitHubRepository[]>(() => {
+    const storagedRepos = localStorage.getItem('@GitCollection:repositories');
+    return storagedRepos ? JSON.parse(storagedRepos) : [];
+  });
   const [newRepo, setNewRepo] = useState('');
+  const [inputError, setInputError] = useState('');
+  const formEl = useRef<HTMLFormElement | null>(null);
+
+  useEffect(() => {
+    localStorage.setItem('@GitCollection:repositories', JSON.stringify(repos));
+  }, [repos]);
 
   function handleInputChange(event: React.ChangeEvent<HTMLInputElement>): void {
     setNewRepo(event.target.value);
@@ -27,12 +36,29 @@ export const Dashboard: React.FC = () => {
     event: React.FormEvent<HTMLFormElement>,
   ): Promise<void> {
     event.preventDefault();
-    const response = await api.get<GitHubRepository>(`repos/${newRepo}`);
 
-    const repository = response.data;
+    if (!newRepo) {
+      setInputError('Informe o username/repositório');
+      return;
+    }
 
-    setRepos([...repos, repository]);
-    setNewRepo('');
+    try {
+      const response = await api.get<GitHubRepository>(`repos/${newRepo}`);
+
+      const repository = response.data;
+
+      const newRepoArr = [...repos, repository];
+      const arrNames = newRepoArr.map(obj => obj.full_name);
+      const filteredRepoArray = newRepoArr.filter(
+        ({ full_name }, index) => !arrNames.includes(full_name, index + 1),
+      );
+
+      setRepos(filteredRepoArray);
+      formEl.current?.reset();
+      setNewRepo('');
+    } catch (err) {
+      setInputError('Repositório privado ou inexistente');
+    }
   }
 
   return (
@@ -40,7 +66,11 @@ export const Dashboard: React.FC = () => {
       <img src={logo} alt="GitCollection" />
       <Title>Catálogo de repositórios do GitHub</Title>
 
-      <Form onSubmit={handleAddRepo}>
+      <Form
+        ref={formEl}
+        hasError={Boolean(inputError)}
+        onSubmit={handleAddRepo}
+      >
         <input
           placeholder="username/repositorie_name"
           value={newRepo}
@@ -49,9 +79,14 @@ export const Dashboard: React.FC = () => {
         <button type="submit">Buscar</button>
       </Form>
 
+      {inputError && <Error>{inputError}</Error>}
+
       <Repos>
         {repos.map(repository => (
-          <a href="/repositories" key={repository.full_name}>
+          <Link
+            to={`/repositories/${repository.full_name}`}
+            key={repository.full_name}
+          >
             <img
               src={repository.owner.avatar_url}
               alt={repository.owner.login}
@@ -62,7 +97,7 @@ export const Dashboard: React.FC = () => {
             </div>
 
             <FiChevronRight size={20} />
-          </a>
+          </Link>
         ))}
       </Repos>
     </>
--- gitcollection/src/pages/Repo/index.tsx
+++ gitcollection/src/pages/Repo/index.tsx
@@ -1,5 +1,101 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
+import { Link, useRouteMatch } from 'react-router-dom';
+import { FiChevronLeft, FiChevronRight } from 'react-icons/fi';
+
+import { api } from '../../services/api';
+import { Header, RepoInfo, Issues } from './styles';
+import logo from '../../assets/svg/logo.svg';
+
+interface RepositoryParams {
+  repository: string;
+}
+
+interface GitHubRepository {
+  full_name: string;
+  description: string;
+  forks_count: number;
+  open_issues_count: number;
+  stargazers_count: number;
+  owner: {
+    login: string;
+    avatar_url: string;
+  };
+}
+
+interface GitHubIssue {
+  id: number;
+  title: string;
+  html_url: string;
+  user: {
+    login: string;
+  };
+}
 
 export const Repo: React.FC = () => {
-  return <h1>Repo</h1>;
+  const [repository, setRepository] = useState<GitHubRepository | null>(null);
+  const [issues, setIssues] = useState<GitHubIssue[]>([]);
+  const { params } = useRouteMatch<RepositoryParams>();
+
+  useEffect(() => {
+    api
+      .get(`repos/${params.repository}`)
+      .then(response => setRepository(response.data));
+
+    api
+      .get(`repos/${params.repository}/issues`)
+      .then(response => setIssues(response.data));
+  }, [params.repository]);
+
+  return (
+    <>
+      <Header>
+        <img src={logo} alt="GitCollection" />
+        <Link to="/">
+          <FiChevronLeft />
+          Voltar
+        </Link>
+      </Header>
+
+      {repository && (
+        <RepoInfo>
+          <header>
+            <img
+              src={repository.owner.avatar_url}
+              alt={repository.owner.login}
+            />
+            <div>
+              <strong>{repository.full_name}</strong>
+              <p>{repository.description}</p>
+            </div>
+          </header>
+          <ul>
+            <li>
+              <strong>{repository.stargazers_count}</strong>
+              <span>Stars</span>
+            </li>
+            <li>
+              <strong>{repository.forks_count}</strong>
+              <span>Forks</span>
+            </li>
+            <li>
+              <strong>{repository.open_issues_count}</strong>
+              <span>Issues</span>
+            </li>
+          </ul>
+        </RepoInfo>
+      )}
+
+      {issues.map(issue => (
+        <Issues key={issue.id}>
+          <a href={issue.html_url} target="_blank" rel="noopener noreferrer">
+            <div>
+              <strong>{issue.user.login}</strong>
+              <p>{issue.title}</p>
+            </div>
+            <FiChevronRight size={20} />
+          </a>
+        </Issues>
+      ))}
+    </>
+  );
 };
